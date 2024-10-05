1d1bf5f5eaab7412db003335573022364aa20b3d
Adrian <adriankon89@gmail.com>
2024-10-05 16:59:56 +0200

    test: Available terms filter add new test and fixed existed

--- tests/unit/AvailableTermsFilterTest.php
+++ tests/unit/AvailableTermsFilterTest.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AvailableTermsFilterTest extends ServiceTestCase
 {
-    public function testAsapAvailableTermsFilter(): void
+    public function testLastAvailableTermsFilter(): void
     {
         //given
         $date = new DateTime('now');
@@ -19,7 +19,7 @@ class AvailableTermsFilterTest extends ServiceTestCase
         $availableTermsEnquiry->setAvailableFrom($date->format('Y-m-d'));
         $availableTermsEnquiry->setAvailableTo($date->format('Y-m-d'));
         $availableTermsEnquiry->setAvailableType('last-available');
-        $bookedTerms = $this->bookedTermsDataProvider();
+        $bookedTerms = $this->bookedTermsDataForTodayProvider();
 
         //when
         $availableTermsFilter = $this->container->get(AvailableTermsFilter::class);
@@ -31,13 +31,78 @@ class AvailableTermsFilterTest extends ServiceTestCase
         $this->assertSame($nextAvailableDay, $tomorrowDate);
     }
 
-    public function bookedTermsDataProvider()
+    public function testAsapFilterReturnsTodayWhenNoBookedTermsExist(): void
+    {
+        //given
+        $date = new DateTime('now');
+        $availableTermsEnquiry = new AvailableTermsEnquiry();
+        $availableTermsEnquiry->setAvailableFrom($date->format('Y-m-d'));
+        $availableTermsEnquiry->setAvailableTo($date->format('Y-m-d'));
+        $availableTermsEnquiry->setAvailableType('asap');
+
+        //when
+        $availableTermsFilter = $this->container->get(AvailableTermsFilter::class);
+        $filteredEnquiry = $availableTermsFilter->apply($availableTermsEnquiry, ...[]);
+
+        //then
+        $nextAvailableDay = $filteredEnquiry->getAvailableDates()[0]->format('Y-m-d');
+        $today = (clone $date)->format('Y-m-d');
+        $this->assertSame($nextAvailableDay, $today);
+    }
+
+    public function testAsapFilterForMoreTimeSheets(): void
+    {
+        //given
+        $date = new DateTime('now');
+        $availableTermsEnquiry = new AvailableTermsEnquiry();
+        $availableTermsEnquiry->setAvailableFrom($date->format('Y-m-d'));
+        $availableTermsEnquiry->setAvailableTo($date->format('Y-m-d'));
+        $availableTermsEnquiry->setAvailableType('asap');
+        $bookedTerms = $this->bookedTermsWithMoreTimeSheetsDataProvider();
+
+        //when
+        $availableTermsFilter = $this->container->get(AvailableTermsFilter::class);
+        $filteredEnquiry = $availableTermsFilter->apply($availableTermsEnquiry, ...$bookedTerms);
+
+        //then
+        $nextAvailableDay = $filteredEnquiry->getAvailableDates()[0]->format('Y-m-d');
+        $tomorrow = (clone $date)->modify('+ 1 day')->format('Y-m-d');
+        $this->assertSame($nextAvailableDay, $tomorrow);
+
+    }
+
+    /**
+     * Provides booked terms for today.
+     *
+     * @return TimeSheet[]
+     */
+    public function bookedTermsDataForTodayProvider()
     {
         $timeSheet = new TimeSheet();
         $date = new DateTime('now');
         $timeSheet->setFromDate($date);
         $timeSheet->setToDate($date);
+
         return [$timeSheet];
+    }
+
+    /**
+     * Provides booked terms for today.
+     *
+     * @return TimeSheet[]
+     */
+    public function bookedTermsWithMoreTimeSheetsDataProvider()
+    {
+        $date = new DateTime('now');
+        $firstTimeSheet = new TimeSheet();
+        $firstTimeSheet->setFromDate($date);
+        $firstTimeSheet->setToDate($date);
+        $futureDate = (clone $date)->modify('+5 days');
+        $secondTimeSheet = new TimeSheet();
+        $secondTimeSheet->setFromDate($futureDate);
+        $secondTimeSheet->setToDate($futureDate);
+
+        return [$firstTimeSheet, $secondTimeSheet];
     }
 
     public function testNotFoundFilter()
